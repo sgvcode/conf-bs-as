8b4187d88e233a7bb3a226710281669d071d6b8d
SGVcode <sgvero.box@gmail.com>
2023-05-20 02:47:09 -0300

    botones descuento sincronizados

--- ticket.js
+++ ticket.js
@@ -25,20 +25,124 @@ document.getElementById('resetBtn').addEventListener('click', function () {
   document.getElementById('totalPagar').innerHTML = 'Total a pagar: $'; // Limpia el párrafo de salida de resultados
 });
 
-// Botones descuento
-var estudianteBtn = document.getElementById('estudianteBtn');
-var traineeBtn = document.getElementById('traineeBtn');
-var juniorBtn = document.getElementById('juniorBtn');
+// ____________________________________________________________
+// Obtener los botones de categoría
+let estudianteBtn = document.getElementById('estudianteBtn');
+let traineeBtn = document.getElementById('traineeBtn');
+let juniorBtn = document.getElementById('juniorBtn');
+let categoriaSelect = document.getElementById('categoria');
+
+// Activa / Desactiva botón de descuento
+function activarBoton(boton) {
+  estudianteBtn.classList.remove('button-active');
+  traineeBtn.classList.remove('button-active');
+  juniorBtn.classList.remove('button-active');
+  boton.classList.add('button-active');
+}
+
+// Activa el botón desde al elegir opción desde <select>
+function activarBotonDesdeSelect() {
+  var categoriaSeleccionada = categoriaSelect.value;
+  if (categoriaSeleccionada === 'estudiante') {
+    activarBoton(estudianteBtn);
+  } else if (categoriaSeleccionada === 'trainee') {
+    activarBoton(traineeBtn);
+  } else if (categoriaSeleccionada === 'junior') {
+    activarBoton(juniorBtn);
+  }
+}
 
 // Escuchar el evento click en los botones de categoría
 estudianteBtn.addEventListener('click', function () {
-  document.getElementById('categoria').value = 'estudiante';
+  categoriaSelect.value = 'estudiante';
+  activarBoton(estudianteBtn);
 });
 
 traineeBtn.addEventListener('click', function () {
-  document.getElementById('categoria').value = 'trainee';
+  categoriaSelect.value = 'trainee';
+  activarBoton(traineeBtn);
 });
 
 juniorBtn.addEventListener('click', function () {
-  document.getElementById('categoria').value = 'junior';
+  categoriaSelect.value = 'junior';
+  activarBoton(juniorBtn);
 });
+
+// Escuchar el evento change en el <select>
+categoriaSelect.addEventListener('change', function () {
+  activarBotonDesdeSelect();
+});
+
+// Activar el botón correspondiente inicialmente
+activarBotonDesdeSelect();
+//_____________________________________________________________
+
+
+// ____________________________________________________________
+// OPCION MOSTRAR TOTAL A PAGAR EN FORMA AUTOMÁTICA
+// Obtener los botones de categoría Automático
+// let estudianteBtn = document.getElementById('estudianteBtn');
+// let traineeBtn = document.getElementById('traineeBtn');
+// let juniorBtn = document.getElementById('juniorBtn');
+// let categoriaSelect = document.getElementById('categoria');
+// let cantidadInput = document.getElementById('cantidad');
+
+// // Función para activar un botón y desactivar los demás
+// function activarBoton(boton) {
+//   estudianteBtn.classList.remove('button-active');
+//   traineeBtn.classList.remove('button-active');
+//   juniorBtn.classList.remove('button-active');
+//   boton.classList.add('button-active');
+// }
+
+// // Función para calcular y actualizar el total a pagar
+// function actualizarTotal() {
+//   var categoria = categoriaSelect.value;
+//   var cantidad = parseFloat(cantidadInput.value);
+//   var descuento = 0;
+
+//   // Descuento
+//   if (categoria === 'estudiante') {
+//     descuento = 0.8;
+//     activarBoton(estudianteBtn);
+//   } else if (categoria === 'trainee') {
+//     descuento = 0.5;
+//     activarBoton(traineeBtn);
+//   } else if (categoria === 'junior') {
+//     descuento = 0.15;
+//     activarBoton(juniorBtn);
+//   }
+
+//   var total = cantidad * 200 * (1 - descuento); // Total
+
+//   var totalPagar = document.getElementById('totalPagar');
+//   totalPagar.innerHTML = `Total a pagar: $ ${total.toFixed(2)}`;
+// }
+
+// // Escuchar el evento click en los botones de categoría
+// estudianteBtn.addEventListener('click', function () {
+//   categoriaSelect.value = 'estudiante';
+//   activarBoton(estudianteBtn);
+//   actualizarTotal();
+// });
+
+// traineeBtn.addEventListener('click', function () {
+//   categoriaSelect.value = 'trainee';
+//   activarBoton(traineeBtn);
+//   actualizarTotal();
+// });
+
+// juniorBtn.addEventListener('click', function () {
+//   categoriaSelect.value = 'junior';
+//   activarBoton(juniorBtn);
+//   actualizarTotal();
+// });
+
+// // Escuchar el evento change en el <select>
+// categoriaSelect.addEventListener('change', function () {
+//   actualizarTotal();
+// });
+
+// // Activar el botón correspondiente inicialmente
+// actualizarTotal();
+// ------------------------------------------------------------------
